716c554790c955a7049918f2dadc220eed2ed849
valentino <valenkvolek@gmail.com>
2025-04-09 15:36:43 -0300

    ....

--- practicas 2023/1/ejer10/target/debug/ejer10.d
+++ practicas 2023/1/ejer10/target/debug/ejer10.d
@@ -1,1 +1,1 @@
-C:\Users\valen\OneDrive\Desktop\UNIVERSIDAD\Rust\practicas\ 2023\1\ejer10\target\debug\ejer10.exe: C:\Users\valen\OneDrive\Desktop\UNIVERSIDAD\Rust\practicas\ 2023\1\ejer10\src\main.rs
+/home/valentino/RustroverProjects/Rust/practicas\ 2023/1/ejer10/target/debug/ejer10: /home/valentino/RustroverProjects/Rust/practicas\ 2023/1/ejer10/src/main.rs
